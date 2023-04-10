edebc3ba9aaa86cbb9bf44bd15af10dc8de8ffb1
404Kurama <kurama.notfound@gmail.com>
2023-04-10 22:59:59 +0700

    feat(main): get game process

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,8 +8,47 @@
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE previousInstance, PSTR arguments, INT commandShow) {
 	// Create Console
 	AllocConsole();
+	freopen_s((FILE**)stdout, "CONIN&", "r", stdin);
 	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
-	std::cout << "Welcome to Melonternal." << std::endl;
+	freopen_s((FILE**)stdout, "CONOUT$", "w", stderr);
+
+	std::cout << "[X] -->> Welcome to Melonternal <<-- [X]" << std::endl;
+
+	// Getting Game Process
+	DWORD processId = Memory::GetProcessId(L"csgo.exe");
+
+	if (!processId) {
+		std::cout << "Waiting for CS:GO..." << std::endl;
+
+		while (!processId) {
+			processId = Memory::GetProcessId(L"csgo.exe");
+			Sleep(200UL);
+		}
+	}
+
+	DWORD client = Memory::GetModuleAddress(processId, L"client.dll");
+	DWORD baseAddress = Memory::GetModuleAddress(processId, L"csgo.exe");
+
+	if (client == 0 || baseAddress == 0) {
+		std::cout << "Waiting for CS:GO to load..." << std::endl;
+
+		while (client == 0 || baseAddress == 0) {
+			client = Memory::GetModuleAddress(processId, L"client.dll");
+			baseAddress = Memory::GetModuleAddress(processId, L"csgo.exe");
+			Sleep(200UL);
+		}
+	}
+
+	// Printing Info
+	std::cout << "----------------------------------------" << std::endl;
+	std::cout << "Base Process Id: " << processId << std::endl;
+	std::cout << "Base Address: " << baseAddress << std::endl;
+	std::cout << "Client Address: " << client << std::endl;
+	std::cout << "----------------------------------------" << std::endl;
+
+	std::cout << "Found game process minimize console in 3s" << std::endl;
+	Sleep(3000UL);
+	ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
 
 	// Create Gui
 	Gui::CreateHWindow("Meloternal", "Meloternal");
