759cceba3b7a96dfd0c4b418160d947b5d89d0a7
404Kurama <kurama.notfound@gmail.com>
2023-04-10 20:50:41 +0700

    feat(gui): add gui header

---
+++ src/Gui.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <d3d9.h>
+
+namespace Gui {
+	// Windows Size
+	constexpr int WIDTH = 500;
+	constexpr int HEIGHT = 300;
+
+	inline bool isRunning = true;
+
+	// Winapi Window Variables
+	inline HWND window = nullptr;
+	inline WNDCLASSEXA windowClass = {};
+
+	// Window Movement
+	inline POINTS position = {};
+
+	// DirectX State Variable
+	inline PDIRECT3D9 d3d = nullptr;
+	inline LPDIRECT3DDEVICE9 device = nullptr;
+	inline D3DPRESENT_PARAMETERS presentParameters = {};
+
+	// Window Creation & Destruction
+	void CreateHWindow(const char* windowName, const char* className) noexcept;
+	void DestroyHWindow() noexcept;
+
+	// Device Creation & Destruction
+	bool CreateDevice() noexcept;
+	void ResetDevice() noexcept;
+	void DestroyDevice() noexcept;
+
+	// ImGui Creation & Destruction
+	void CreateImGui() noexcept;
+	void DestroyImGui() noexcept;
+
+	// Render
+	void BeginRender() noexcept;
+	void EndRender() noexcept;
+	void Render() noexcept;
+}
