9bccdebd7d616e2cb03d42799d56341d43ea4226
404Kurama <kurama.notfound@gmail.com>
2023-04-10 20:59:41 +0700

    feat(main): init

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,27 @@
+#include "Gui.h"
+
 #include <Windows.h>
+#include <thread>
 
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE previousInstance, PSTR arguments, INT commandShow) {
+	// Create Gui
+	Gui::CreateHWindow("Gay", "GayClass");
+	Gui::CreateDevice();
+	Gui::CreateImGui();
+
+	// Render Loop
+	while (Gui::isRunning) {
+		Gui::BeginRender();
+		Gui::Render();
+		Gui::EndRender();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	// Destroy Gui
+	Gui::DestroyImGui();
+	Gui::DestroyDevice();
+	Gui::DestroyHWindow();
+	
 	return 0;
 }
