54a821780eb06aa9a212a85735540cfcf3f45111
404Kurama <kurama.notfound@gmail.com>
2023-04-11 01:00:35 +0700

    feat(modules): add bhop moule

--- src/Modules.cpp
+++ src/Modules.cpp
@@ -3,10 +3,27 @@
 #include "Globals.h"
 #include "Offsets.hpp"
 
+#include <iostream>
 #include <thread>
 
 void Modules::MovementThread() noexcept {
 	while (Gui::isRunning) {
+		const auto localPlayer = Memory::Read<uintptr_t>(Globals::processHandle, Globals::clientAddress + Offsets::signatures::dwLocalPlayer);
+
+		if (localPlayer) {
+			const auto health = Memory::Read<std::int32_t>(Globals::processHandle, localPlayer + Offsets::netvars::m_iHealth);
+			const auto lifeState = Memory::Read<std::int32_t>(Globals::processHandle, localPlayer + Offsets::netvars::m_lifeState);
+			const auto flags = Memory::Read<uintptr_t>(Globals::processHandle, localPlayer + Offsets::netvars::m_fFlags);
+
+			if (GetAsyncKeyState(VK_SPACE) && health > 0 && lifeState == 0 && Globals::bhop) {
+				if (flags & (1 << 0)) {
+					Memory::Write<uintptr_t>(Globals::processHandle, Globals::clientAddress + Offsets::signatures::dwForceJump, 6);
+				}
+				else {
+					Memory::Write<uintptr_t>(Globals::processHandle, Globals::clientAddress + Offsets::signatures::dwForceJump, 4);
+				}
+			}
+		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 }
