45d5c1111a70576bceb619718c37b1191300f9b6
404Kurama <kurama.notfound@gmail.com>
2023-04-10 21:49:46 +0700

    feat(globals): add globals header

---
+++ src/Globals.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+namespace globals {
+	// Address
+	inline std::uintptr_t clientAddress = 0;
+
+	// Modules
+	inline bool bhop = false;
+	inline bool glow = false;
+
+	inline float glowColors[] = { 1.f, 0.f, 0.f, 1.f };
+}
